24472130b90a75f2e50bf407bb319223dd40fad1
Alex-D <contact@alex-d.fr>
2023-09-13 15:36:52 +0200

    feat: add eslint-plugin-n recommended to rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
 	},
 	extends: [
 		'eslint:recommended',
+		'plugin:n/recommended',
 		'plugin:@typescript-eslint/eslint-recommended',
 		'plugin:@typescript-eslint/recommended',
 	],
